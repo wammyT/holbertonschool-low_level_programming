f48ee15f6794d47a69dc8127f6149e35bd6f2a16
tamara walling <tamara_walling@yahoo.com>
2023-10-10 10:15:17 -0700

    spelling

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry Point
  *
- * Return: Alwasy 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multtilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
