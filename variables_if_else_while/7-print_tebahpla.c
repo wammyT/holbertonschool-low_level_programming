c90d6b003fa888449e20c07cbeed638af827c70a
tamara walling <tamara_walling@yahoo.com>
2023-10-10 12:11:02 -0700

    betty

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse. 
+ * main - prints the lowercase alphabet in reverse.
  * followed by a new line
  *
  * Return: Always 0 (Success)
